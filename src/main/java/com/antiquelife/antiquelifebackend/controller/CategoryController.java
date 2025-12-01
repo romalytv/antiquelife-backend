fb66a5cd8991c738649e729c83d21c8069cc7523
romalytv <youlikeyoupass@gmail.com>
2025-12-01 19:04:10 +0200

    Added Category Controller For DB

---
+++ src/main/java/com/antiquelife/antiquelifebackend/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.antiquelife.antiquelifebackend.controller;
+
+import com.antiquelife.antiquelifebackend.entity.Category;
+import com.antiquelife.antiquelifebackend.repo.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+}
