20014ad7493ffc6312a5ecd6e640ff976b6fdc5d
romalytv <youlikeyoupass@gmail.com>
2025-12-04 17:54:23 +0200

    Backing up

--- src/main/java/com/antiquelife/antiquelifebackend/configs/SecurityConfig.java
+++ src/main/java/com/antiquelife/antiquelifebackend/configs/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig {
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
